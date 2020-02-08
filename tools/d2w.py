18125f0eb367f9ff0276c10702bc6ae050581a17
guodong <guod.jin@gmail.com>
2020-02-07 21:40:48 -0500

    update events json format and tools

--- tools/d2w.py
+++ tools/d2w.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
-response = requests.get('https://2019-n-cov.github.io/').text
+response = requests.get('https://2019-ncov-wuhan.github.io/').text
 
 soup = BeautifulSoup(response, 'html.parser')
 
@@ -17,16 +17,26 @@ with open('../data/events.json', 'r') as input:
 		item_soup = BeautifulSoup("""<li class="timeline-item bg-white rounded ml-3 p-4 shadow"><div class="timeline-arrow"></div><h2 class="h5 mb-0"></h2></li>""", 'html.parser')
 		date_tag = item_soup.find("h2")
 		date_tag.string = item['date']
+		for word in item['keywords']:
+			word_soup = BeautifulSoup("""<span class="small text-gray"></span>""", 'html.parser')
+			word_soup.span.string = word
+			item_soup.li.append(word_soup)
 		for event in item['events']:
 			event_soup = BeautifulSoup("""<p class="text-small mt-2 font-weight-light"></p>""", 'html.parser')
 			event_soup.p.string = event['description']
-			# link_ = event['link']
-			if ('link' in event):
-				link_soup = BeautifulSoup("""<a href="">链接</a>""", 'html.parser')
-				link_soup.a['href']=event['link']
+			link_count = 1
+			for link in event['links']:
+				link_soup = BeautifulSoup("""<a href="">链接""" + str(link_count) + """</a>""", 'html.parser')
+				link_soup.a['href']=link
 				event_soup.p.append(link_soup)
+			if ('img' in event):
+				img_soup = BeautifulSoup("""<img src="" style="width:-webkit-fill-available"/>""", 'html.parser')
+				img_soup.img['src']="https://2019-ncov-wuhan.github.io/imgs/"+event['img']
+				event_soup.p.append(img_soup)
+			hr_soup = BeautifulSoup("""<hr/>""", 'html.parser')
 			item_soup.li.append(event_soup)
-
+			item_soup.li.append(hr_soup)
+		item_soup.find_all("hr")[-1].decompose()
 		items_tag.insert(index, item_soup)
 		index = index+1
 
--- tools/w2d.py
+++ tools/w2d.py
@@ -7,7 +7,7 @@ import json
 data = {}
 data['items'] = []
 
-response = requests.get('https://2019-n-cov.github.io/').text
+response = requests.get('https://2019-ncov-wuhan.github.io/').text
 
 soup = BeautifulSoup(response, 'html.parser')
 
@@ -15,14 +15,19 @@ items = soup.find_all('li', class_="timeline-item")
 for item in items:
 	item_json = {}
 	item_json['date'] = item.h2.get_text().strip()
+	item_json['keywords'] = []
 	item_json['events'] = []
 	
 	for event in item.find_all('p'):
 		event_json = {}
 		event_json['description'] = event.get_text().strip().replace('\n', '').replace(' ', '').replace('链接', '')
 		link = event.find('a')
+		event_json['links'] = []
+		img = event.find('img')
 		if link != None:
-			event_json['link'] = link['href'].strip()
+			event_json['links'].append(link['href'].strip())
+		if img != None:
+			event_json['img'] = img['src'].strip()
 		item_json['events'].append(event_json)
 
 	data['items'].append(item_json)
